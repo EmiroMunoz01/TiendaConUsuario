0ff4c39d67edc28764eb090dda9aab0e2d15b166
Jake <emiro_2001@icloud.com>
2025-07-27 16:21:57 -0500

    Proyecto de Tienda

---
+++ Tienda/src/main/java/org/example/tienda/modelo/producto/Producto.java
@@ -0,0 +1,27 @@
+package org.example.tienda.modelo.producto;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Producto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nombre;
+    private Integer cantidad;
+    private Boolean disponibilidad;
+
+}
