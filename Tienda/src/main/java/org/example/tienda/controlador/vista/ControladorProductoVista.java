c6ed73f69bfc18631b5586baaceddc24de9aa583
Jake <emiro_2001@icloud.com>
2025-08-02 18:18:07 -0500

    Se han creado todos los metodos necesarios para que el CRUD funcione, ademas de implementar buenas practicas al momento de la finalizacion. Se ha usado solo una entidad para la iteraccion.

--- Tienda/src/main/java/org/example/tienda/controlador/vista/ControladorProductoVista.java
+++ Tienda/src/main/java/org/example/tienda/controlador/vista/ControladorProductoVista.java
@@ -3,15 +3,16 @@ package org.example.tienda.controlador.vista;
 
 import jakarta.validation.Valid;
 import org.example.tienda.dto.producto.administrador.CrearProductoAdministradorDTO;
+import org.example.tienda.dto.producto.administrador.EditarProductoAdministradorDTO;
+import org.example.tienda.dto.producto.administrador.ObtenerProductosAdministradorDTO;
+import org.example.tienda.modelo.producto.Producto;
 import org.example.tienda.servicio.producto.ServicioProducto;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/tienda/producto")
@@ -39,6 +40,39 @@ public class ControladorProductoVista {
         servicioProducto.crearProductoAdministrador(producto);
         return "redirect:/tienda/producto/listar";
 
+    }
+
+
+    @GetMapping("/editar/{id}")
+    public String mostrarFormularioEditar(Model model, @PathVariable int id) {
+        ObtenerProductosAdministradorDTO producto = servicioProducto.obtenerProductoPorIdAdministrador(id);
+
+        model.addAttribute("producto", producto);
+
+        return "producto/editar";
+    }
+
+
+    @PostMapping("/editar/id/{id}")
+    public String procesarEdicionProducto(@PathVariable Integer id,
+                                          @ModelAttribute("producto") @Valid EditarProductoAdministradorDTO dto,
+                                          BindingResult result,
+                                          RedirectAttributes redirectAttributes) {
+        if (result.hasErrors()) {
+            // Volver al formulario con errores
+            return "producto/editar";
+        }
+
+        servicioProducto.editarProductoPorIdAdministrador(dto, id);
+        redirectAttributes.addFlashAttribute("mensaje", "Producto actualizado correctamente");
+        return "redirect:/tienda/producto/listar";  // Rediriges a la lista tras actualizar
+    }
+
+    @PostMapping("/eliminar/{id}")
+    public String eliminarProducto(@PathVariable Integer id, RedirectAttributes redirectAttributes) {
+        servicioProducto.eliminarProductoPorIdAdministrador(id);
+        redirectAttributes.addFlashAttribute("mensaje", "Producto eliminado correctamente");
+        return "redirect:/tienda/producto/listar"; // redirige a la lista de productos
     }
 
 
